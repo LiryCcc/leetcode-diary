68617f862938ebb96dbea8ac8cca771a357ad0b0
Liry <learainccc@outlook.com>
2025-05-07 21:38:18 +0800

    增加超时时间

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 const vitestConfig = defineConfig({
   test: {
     // ...
-    testTimeout: 300000
+    testTimeout: 3000000
   }
 });
 export default vitestConfig;
