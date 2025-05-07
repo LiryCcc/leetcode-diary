879d6f0fc7ccf44bee4f4676b83116f2076af8dd
Liry <learainccc@outlook.com>
2025-05-07 21:32:31 +0800

    完成2025-05-07每日一题3341

--- leetcode/utils.ts
+++ leetcode/utils.ts
@@ -7,4 +7,20 @@ function generateRandomArray(length: number, min: number, max: number) {
   return result;
 }
 
-export { generateRandomArray };
+function generateRandom2DArray(n: number, m: number, min: number, max: number): number[][] {
+  if (min > max) {
+    throw new Error('最小值不能大于最大值');
+  }
+
+  const array: number[][] = [];
+  for (let i = 0; i < n; i++) {
+    const row: number[] = [];
+    for (let j = 0; j < m; j++) {
+      row.push(Math.floor(Math.random() * (max - min + 1)) + min);
+    }
+    array.push(row);
+  }
+  return array;
+}
+
+export { generateRandom2DArray, generateRandomArray };
