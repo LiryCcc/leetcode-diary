de691dc00021812167ade93be804f2c0c5b76006
Liry <learainccc@outlook.com>
2025-08-14 15:55:44 +0800

    增加测试线程数

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,17 @@
+import { cpus } from 'node:os';
 import { defineConfig } from 'vitest/config';
 
 const vitestConfig = defineConfig({
   test: {
     // ...
-    testTimeout: 30000000
+    testTimeout: 30000000,
+    pool: 'threads',
+    poolOptions: {
+      threads: {
+        minThreads: cpus().length,
+        maxThreads: cpus().length
+      }
+    }
   }
 });
 export default vitestConfig;
