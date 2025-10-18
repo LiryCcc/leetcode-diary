7227a05077149bcaca1fe7decb4f7a0a9587c078
Liry <learainccc@outlook.com>
2025-10-18 16:45:48 +0800

    增加新功能

--- cspell.config.js
+++ cspell.config.js
@@ -4,7 +4,7 @@ const cspellConfig = defineConfig({
   version: '0.2',
   useGitignore: true,
   ignorePaths: ['pnpm-lock.yaml', '*3541*'],
-  words: ['nums', 'liry', 'datastructures', 'leetcode', 'Subarrays', 'psum', 'radash', 'arrs', 'aeiou']
+  words: ['nums', 'liry', 'datastructures', 'leetcode', 'Subarrays', 'psum', 'radash', 'arrs', 'aeiou', 'overscan']
 });
 
 export default cspellConfig;
